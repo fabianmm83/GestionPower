2614e827fdbe3b6a22574b439ce1e9a6c42e9aad
Fabian <fabianmm83@otmail.com>
2025-03-09 20:33:49 -0600

    Modificación para aplicar migraciones antes de iniciar la app en Render

--- migrate.py
+++ migrate.py
@@ -1,7 +1,9 @@
 from app import create_app
 from flask_migrate import Migrate, upgrade
 
+# Crear la aplicación
 app = create_app()
 
+# Ejecutar las migraciones dentro del contexto de la aplicación
 with app.app_context():
     upgrade()  # Esto aplica las migraciones pendientes
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,11 @@
 import os
 from app import create_app
-from migrate import migrate
 
+# Crear la aplicación
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        migrate()
-        
-        
+    # Obtener el puerto de la variable de entorno (usado por Render)
     port = int(os.environ.get("PORT", 5000))  # Render generalmente asigna un puerto dinámico
     print(f"Corriendo en: http://127.0.0.1:{port}")
     app.run(host="0.0.0.0", port=port, debug=True)  # No se usa en producción pero se deja por si acaso
