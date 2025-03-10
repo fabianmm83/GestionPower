6bd25906f1596d0b3700b18695118f345a44297f
Fabian <fabianmm83@otmail.com>
2025-03-09 18:18:34 -0600

    Corrección de wsgi.py para producción en Render

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Usa el puerto dinámico o 5000 por defecto
     print(f"Servidor corriendo muy bien en: http://127.0.0.1:{port}")
-    app.run(host="0.0.0.0", port=port, debug=True)  # Usa el servidor de desarrollo de Flask
+    app.run(host="0.0.0.0", port=port, debug=False)  # Usa el servidor de desarrollo de Flask
