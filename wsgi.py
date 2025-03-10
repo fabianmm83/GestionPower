77564c48f2204991af6fd05864c4d5aa71898028
Fabian <fabianmm83@otmail.com>
2025-03-09 20:27:40 -0600

    Agregado el script para ejecutar migraciones automáticamente en wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 import os
 from app import create_app
+from migrate import migrate
 
 app = create_app()
 
 if __name__ == "__main__":
+    with app.app_context():
+        migrate()
+        
+        
     port = int(os.environ.get("PORT", 5000))  # Render generalmente asigna un puerto dinámico
     print(f"Corriendo en: http://127.0.0.1:{port}")
     app.run(host="0.0.0.0", port=port, debug=True)  # No se usa en producción pero se deja por si acaso
