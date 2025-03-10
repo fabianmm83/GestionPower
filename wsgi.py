d1b59290c25b7ef5ed5391bd59dfb01b35c29f6a
Fabian <fabianmm83@otmail.com>
2025-03-09 18:59:48 -0600

    Actualización de wsgi.py y requirements.txt

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
-# No es necesario usar app.run() en producción
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render generalmente asigna un puerto dinámico
+    app.run(host="0.0.0.0", port=port, debug=False)  # No se usa en producción pero se deja por si acaso
